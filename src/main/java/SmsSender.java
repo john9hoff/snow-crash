1d0537f625036131981b676b008e24bb05c25599
Chris Horton <Chris.horton@sbcglobal.net>
2018-07-31 11:04:14 -0500

    add api keys

--- src/main/java/SmsSender.java
+++ src/main/java/SmsSender.java
@@ -6,16 +6,16 @@ import com.twilio.type.PhoneNumber;
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
     public static final String ACCOUNT_SID =
-            "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            "AC4c726d0ba0e16d5121d2f55e073acb5e";
     public static final String AUTH_TOKEN =
-            "your_auth_token";
+            "61db361d0f0e58b2674050d8686a01b7";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
-                .creator(new PhoneNumber("+14159352345"), // to
-                        new PhoneNumber("+14158141829"), // from
+                .creator(new PhoneNumber("+19257862700"), // to
+                        new PhoneNumber("+"), // from
                         "Where's Wallace?")
                 .create();
 
